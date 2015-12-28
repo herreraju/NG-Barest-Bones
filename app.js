8329f48b3a9199c4c354b39451784d49067a2488
Mike McGuire <MoonshotMike@gmail.com>
2015-12-28 05:58:00 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// MODULE
+var myApp = angular.module('myApp', []);
+
+// CONTROLLER
+myApp.controller('mainController', ['$scope', function ($scope) 
+    {
+    $scope.name = "Mullen";
+    }]);
+
+
+
+
+
